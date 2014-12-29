dbdb132549abc5e925ea424f8d9dd71ff6ff8120
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-29 14:05:43 -0500

    include the asset_host logic in the Mailer

--- spec/lib/roadie/rails/mailer_spec.rb
+++ spec/lib/roadie/rails/mailer_spec.rb
@@ -4,8 +4,8 @@ require 'mail'
 module Roadie
   module Rails
     describe Mailer do
-      some_mailer = Class.new do
-        include Mailer
+      base_mailer = Class.new do
+        cattr_accessor :asset_host
 
         def initialize(email = nil)
           @email = email
@@ -14,6 +14,10 @@ module Roadie
         def mail(options = {})
           @email
         end
+      end
+
+      some_mailer = Class.new(base_mailer) do
+        include Mailer
       end
 
       describe "#roadie_options" do
@@ -50,6 +54,34 @@ module Roadie
           expect(MailInliner).to receive(:new).with(email, instance_of(Options)).and_return inliner
           expect(inliner).to receive(:execute).and_return "inlined email"
           expect(instance.roadie_mail).to eq("inlined email")
+        end
+      end
+
+      describe "#asset_host" do
+        it "passes all arguments to the underlying simple accessor" do
+          string_host_mailer = Class.new(base_mailer) do
+            self.asset_host = 'http://original.com'
+
+            include Automatic
+          end
+
+          asset_host = string_host_mailer.asset_host
+          expect(asset_host).to be_a(Proc)
+          expect(asset_host.call('foo.png')).to eq('http://original.com')
+        end
+
+        it "passes all arguments to the underlying proc accessor" do
+          proc_host_mailer = Class.new(base_mailer) do
+            self.asset_host = Proc.new { |source, request|
+              'http://original.com'
+            }
+
+            include Automatic
+          end
+
+          asset_host = proc_host_mailer.asset_host
+          expect(asset_host).to be_a(Proc)
+          expect(asset_host.call('foo.png')).to eq('http://original.com')
         end
       end
     end

d206c2c224179834306a2e71aeda885f9fc6675d
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-29 14:06:02 -0500

    extract asset_host specs

--- spec/lib/roadie/rails/mailer_spec.rb
+++ spec/lib/roadie/rails/mailer_spec.rb
@@ -56,34 +56,6 @@ module Roadie
           expect(instance.roadie_mail).to eq("inlined email")
         end
       end
-
-      describe "#asset_host" do
-        it "passes all arguments to the underlying simple accessor" do
-          string_host_mailer = Class.new(base_mailer) do
-            self.asset_host = 'http://original.com'
-
-            include Automatic
-          end
-
-          asset_host = string_host_mailer.asset_host
-          expect(asset_host).to be_a(Proc)
-          expect(asset_host.call('foo.png')).to eq('http://original.com')
-        end
-
-        it "passes all arguments to the underlying proc accessor" do
-          proc_host_mailer = Class.new(base_mailer) do
-            self.asset_host = Proc.new { |source, request|
-              'http://original.com'
-            }
-
-            include Automatic
-          end
-
-          asset_host = proc_host_mailer.asset_host
-          expect(asset_host).to be_a(Proc)
-          expect(asset_host.call('foo.png')).to eq('http://original.com')
-        end
-      end
     end
   end
 end

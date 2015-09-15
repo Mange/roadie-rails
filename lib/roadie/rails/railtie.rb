78831735702ad00a9fb91937b881c019d403c02d
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-09-15 12:21:41 -0300

    Configure sprockets-rails 3 correctly

--- lib/roadie/rails/railtie.rb
+++ lib/roadie/rails/railtie.rb
@@ -13,8 +13,14 @@ module Roadie
         # Saying config.assets.enabled here does not work in Rails 3.1-3.2, but
         # APP.config.assets work. There is a difference between "config" and
         # "app.config" on those versions.
-        if app.config.respond_to?(:assets) && app.config.assets.enabled != false && app.assets
-          config.roadie.asset_providers << AssetPipelineProvider.new(app.assets)
+        if app.config.respond_to?(:assets) && app.config.assets.enabled != false
+          if app.assets
+            config.roadie.asset_providers << AssetPipelineProvider.new(app.assets)
+          else
+            app.config.assets.configure do |env|
+              config.roadie.asset_providers << AssetPipelineProvider.new(env)
+            end
+          end
         end
       end
     end

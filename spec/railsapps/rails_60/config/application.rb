7626562e6ad86af1e7d2bbc30bdc7ed85c6016d8
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-03-31 15:35:32 +0100

    Relax Rails dependency to < 6.1
    Rails 6.0 is about to be released

---
+++ spec/railsapps/rails_60/config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require "active_model/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Rails60
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.roadie.url_options = { host: 'example.app.org' }
+  end
+end

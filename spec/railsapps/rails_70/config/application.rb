94f0d8418fbeecc7ee2e425b3f2f054862a9a3da
Sean Floyd <sean@hivebrite.com>
2021-11-22 22:15:14 +0100

    missing files

--- spec/railsapps/rails_70/config/application.rb
+++ spec/railsapps/rails_70/config/application.rb
@@ -1,15 +1,18 @@
 require_relative "boot"
 
 require "rails"
+# Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
 # require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-require "action_view/railtie"
 require "action_mailer/railtie"
-# require "active_job/railtie"
-# require "action_cable/engine"
 # require "action_mailbox/engine"
 # require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -28,5 +31,6 @@ module Rails70
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.roadie.url_options = {host: 'example.app.org'}
   end
 end

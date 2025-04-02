ecc90ad8e3455aab93f833589ec5d0bfde618e0e
PikachuEXE <git@pikachuexe.net>
2025-04-02 09:33:28 +0800

    ! Fix rails app for testing for ruby 3.4 & 3.5

--- spec/railsapps/rails_70/config/boot.rb
+++ spec/railsapps/rails_70/config/boot.rb
@@ -1,4 +1,6 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+# Workaround for uninitialized constant ActiveSupport::LoggerThreadSafeLevel::Logger
+require "logger"
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
